576b9b014ce06c9d0c4b47a2d83486389d433c07
Harold Martin Patacsil <90546802+yskooo@users.noreply.github.com>
2023-09-08 20:14:57 +0800

    feat: vercel analytics

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+import { Analytics } from '@vercel/analytics/react';
+
 import Feature from "../components/Feature";
 import Pricing from "../components/Pricing";
 import Hero from "../components/Hero";
@@ -7,6 +9,7 @@ import SeoHead from "../components/SeoHead";
 export default function Home() {
   return (
     <>
+      <Analytics />
       <SeoHead title="Mang Rey's Mushroom Chicharon" />
       <Layout>
         <Hero />
