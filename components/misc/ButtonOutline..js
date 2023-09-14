16bdb6c074bcbf4f5545c04e2f83688f9c2b3ce6
Pam Patacsil <74691394+patacsilpam@users.noreply.github.com>
2023-09-14 13:47:22 +0000

    chore:content

--- components/misc/ButtonOutline..js
+++ components/misc/ButtonOutline..js
@@ -3,10 +3,9 @@ import React from "react";
 const ButtonOutline = ({ children }) => {
   return (
     <button
-    href="https://www.facebook.com/profile.php?id=100092878066198" 
     taget="_blank"
     className="font-medium tracking-wide py-2 px-5 sm:px-8 border border-[#968851] text-[#968851] active:bg-[#968851] outline-none rounded-l-full rounded-r-full capitalize bg-white-500 hover:bg-[#968851] hover:text-white-500 transition-all hover:shadow-[#968851] ">
-      {" "}
+      
       {children}
     </button>
   );
